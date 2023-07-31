09b1d4848fe98f3257a2ad90789ca40a39a81b34
FFengIll <fengyouzheng@gmail.com>
2023-07-31 12:35:10 +0800

    chore: add a simple example.

---
+++ example/simple/simple.py
@@ -0,0 +1,28 @@
+from typing import List
+from pydantic import BaseModel
+
+from typechatpy.translator import Translator
+
+
+class VenueData(BaseModel):
+    venue: str
+    description: str
+
+
+class Response(BaseModel):
+    data: List[VenueData]
+
+
+prompt = "Provide 3 suggestions for specific places to go to in Seattle on a rainy day."
+
+
+def main():
+    t = Translator()
+    fmt = t.process(Response, VenueData)
+
+    res = t.generate(prompt, fmt)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
