c476d898dbbd90ed114cdd48fee807f636899d7d
FFengIll <FFengIll@users.noreply.github.com>
2025-02-13 03:11:34 +0000

    feat: update example.

---
+++ example/auto/main.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from typechatpy.translator import Translator
+
+
+class VenueData(BaseModel):
+    venue: str
+    description: str
+
+
+class Response(BaseModel):
+    data: List[VenueData]
+
+
+prompt = "Provide 3 suggestions for specific places to go to in Seattle on a rainy day."
+
+
+def main():
+    t = Translator()
+
+    res = t.generate(prompt, auto=True)
+
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
--- example/model/model.py
+++ example/model/model.py
@@ -2,7 +2,6 @@ from typing import List
 from pydantic import BaseModel
 
 
-
 class VenueData(BaseModel):
     venue: str
     description: str
